19bf20e52f53115b4d116189753686b3f6962dc8
Josef Reichelt <pepareichelt@gmail.com>
2019-07-06 11:03:25 +0200

    Updates

---
+++ precache-manifest.04e41d15635deb316c2ff2c1a2796f37.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9af3015f98ef4fb1cb95165cb08f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd88acbd8acb049bfcc",
+    "url": "/static/css/main.dd5cf2fd.chunk.css"
+  },
+  {
+    "revision": "be6ffd899fd414774830",
+    "url": "/static/js/2.d34c9c7a.chunk.js"
+  },
+  {
+    "revision": "9fd88acbd8acb049bfcc",
+    "url": "/static/js/main.bb51181b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "851de7c6ae64ac8cabaef1683aa45542",
+    "url": "/static/media/artstationMono.851de7c6.svg"
+  },
+  {
+    "revision": "fd84d269778e59a2bf36a380d270c487",
+    "url": "/static/media/brickthecastle.fd84d269.png"
+  },
+  {
+    "revision": "521dd77937b0013ab2c3807454437a6e",
+    "url": "/static/media/emailMono.521dd779.svg"
+  },
+  {
+    "revision": "9c45886733d27fdf2c0148306badcab9",
+    "url": "/static/media/foto.9c458867.jpg"
+  },
+  {
+    "revision": "aa8efd7ac16bc69ade897a84ee1e7fc9",
+    "url": "/static/media/gdcs.aa8efd7a.png"
+  },
+  {
+    "revision": "fb2439cd6a9e35ced87e0e778fc66caf",
+    "url": "/static/media/githubMono.fb2439cd.svg"
+  },
+  {
+    "revision": "3596cd590b53a8ab3c3b9ceca224faee",
+    "url": "/static/media/kybernauts.3596cd59.png"
+  },
+  {
+    "revision": "4d34d9ed363f03939a22a6d7406543d5",
+    "url": "/static/media/logoMono.4d34d9ed.svg"
+  },
+  {
+    "revision": "d52fe3804aeb3ea414acf82a345fcb5a",
+    "url": "/static/media/sheriffpac.d52fe380.png"
+  },
+  {
+    "revision": "74d073a7dafa29f824f8364bfeea3748",
+    "url": "/static/media/sketchfabMono.74d073a7.svg"
+  },
+  {
+    "revision": "200dc06dc811570e1f444808eef72f68",
+    "url": "/static/media/stamperman.200dc06d.jpg"
+  },
+  {
+    "revision": "1ca6efd7932404bb79b026c241a961ca",
+    "url": "/static/media/twitterMono.1ca6efd7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b68f0dbe5808ad59f661ec007b8c101.js"
+  "/precache-manifest.04e41d15635deb316c2ff2c1a2796f37.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
