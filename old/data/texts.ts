a4ba9af1132e13ba8a202f785307c705a5e3c7be
Josef Reichelt <pepareichelt@gmail.com>
2021-04-29 18:36:29 +0200

    Remove old version

--- old/data/texts.ts
+++
@@ -1,6 +0,0 @@
-import { calculateAge } from "../utils/ageCalc";
-
-
-export const motto = "web developer, designer, game maker";
-export const info = `Jmenuji se Josef Reichelt, je mi ${calculateAge(new Date(1992, 7, 27))}let a jsem nadšenec do vývoje her a software.`;
-export const currentInterests = "V současné chvíli se specializuji na vývoj front-end aplikací, které používají React, Typescript, Next.js, Styled-Components. Zabývám se ale i jinými technologiemi jako Vue, Svelte a lit-html.";
--- old/utils/ageCalc.ts
+++
@@ -1,5 +0,0 @@
-export const calculateAge = (date: Date): number => {
-    const diff = Date.now() - date.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getFullYear() - 1970);
-};
