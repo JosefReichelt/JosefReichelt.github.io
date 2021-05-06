e037c6f2a53e5897adb5e90ca1b196ce1317a89c
Josef Reichelt <pepareichelt@gmail.com>
2021-05-06 19:18:41 +0200

    Add pexeso project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,16 @@
+import pexesoImage from '_assets/projects/pexeso.jpg';
 import brickImage from '_assets/projects/brickthecastle.jpg';
 import pacImage from '_assets/projects/sheriffpac.jpg';
 
 
 export const projects: IProject[] = [
+    {
+        name: 'Pexeso',
+        desc: 'Jednoduchá pexeso hra vytvořená v Reactu.',
+        image: pexesoImage,
+        link: 'https://josefreichelt.eu/pexeso',
+        tech: [ 'React', 'Typescript' ]
+    },
     {
         name: 'Brick the Castle',
         desc: 'Klon Arkanoidu  s trochou úprav vytvořený v Unity během 72 hodin pro GDL GameJam.',
