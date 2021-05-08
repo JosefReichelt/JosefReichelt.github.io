9da1c8a87a2a32c9b86cf508dfb2e47c6c9c3483
Josef Reichelt <pepareichelt@gmail.com>
2021-05-08 07:46:23 +0200

    Addd manifest

--- config/webpack.plugins.ts
+++ config/webpack.plugins.ts
@@ -4,9 +4,12 @@ import BundleAnalyzer from 'webpack-bundle-analyzer/';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import SpriteLoaderPlugin from 'svg-sprite-loader/plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 
-import { PUBLIC_PATH, IS_ANALYZE } from './constants';
+import {
+    PUBLIC_PATH, IS_ANALYZE, BUILD_PATH,
+} from './constants';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 
@@ -26,6 +29,15 @@ export const plugins = (): WebpackPluginInstance[] => {
             favicon: resolve(PUBLIC_PATH, 'favicon.ico')
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: PUBLIC_PATH,
+                    to: BUILD_PATH,
+                    globOptions: { ignore: [ '*.html', '.*ico' ] }
+                }
+            ]
+        })
     ];
     if (IS_ANALYZE){
         plugins.push( new BundleAnalyzer.BundleAnalyzerPlugin() );
